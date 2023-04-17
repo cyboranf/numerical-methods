21c2b9c64fdc4bafc1410f7675e159ea92f08a5f
Wiktoria Assman <wa117777@stud.ur.edu.pl>
2023-04-17 12:22:03 +0200

    traction -done

---
+++ src/python/traction.py
@@ -0,0 +1,20 @@
+def f(x):
+    return x ** 2 - 1
+
+
+s = 0
+
+xp = int(input("podja pocztek: "))
+
+xk = int(input("podja koniec: "))
+
+n = 100000
+
+dx = (xk - xp) / n
+
+for i in range(n):
+    s = s + f(xp + i * dx)
+
+s = (s + (f(xp) + f(xk) / 2)) * dx
+
+print(s)
